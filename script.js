fc41b22b99fb992ac20d88cadb31ecab76a07ff6
amen stoney <amenstoney@gmail.com>
2025-07-15 15:39:40 -0400

    Edited js and html for demo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Future Code 2 GitHub Practice File
 
 console.log('Welcome to GitHub practice!');
-console.log('Try editing this message and pushing your changes.');
+console.log('Its every day bro');
